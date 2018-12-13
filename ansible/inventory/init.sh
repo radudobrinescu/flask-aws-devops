551999597e9dcaae7a4046125a943f35a592fc80
Radu Dobrinescu <radu.dobrinescu@gmail.com>
2018-12-13 23:54:12 +0200

    added uwsgi service

--- ansible/inventory/init.sh
+++ ansible/inventory/init.sh
@@ -15,4 +15,9 @@ sudo su -c 'source venv/bin/activate' - ubuntu
 sudo su -c 'pip install -r /var/www/devops_app/app/requirements.txt' - ubuntu
 sudo mkdir /var/log/uwsgi
 sudo chown ubuntu:ubuntu /var/log/uwsgi
-uwsgi --ini /var/www/devops_app/web/devops_uwsgi.ini
+sudo mv /var/www/devops_app/web/uwsgi.conf /etc/init
+sudo mkdir /etc/uwsgi && sudo mkdir /etc/uwsgi/vassals
+sudo ln -s /var/www/devops_app/web/devops_uwsgi.ini /etc/uwsgi/vassals
+sudo chown -R ubuntu:ubuntu /var/log/uwsgi/
+sudo start uwsgi
+#uwsgi --ini /var/www/devops_app/web/devops_uwsgi.ini
