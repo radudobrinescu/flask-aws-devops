43fb88256cade0192169397e491c2309cdba9b66
Radu Dobrinescu <radu.dobrinescu@gmail.com>
2018-12-15 14:57:14 +0200

    changed store to dynamodb

--- app/app.py
+++ app/app.py
@@ -1,6 +1,13 @@
 from flask import Flask, jsonify, json, Response, request
 from datetime import datetime as dt
-import os
+from boto3.dynamodb.conditions import Key, Attr
+import boto3
+
+dynamodb = boto3.resource('dynamodb', region_name='eu-central-1',
+                          aws_access_key_id='AKIAIK6LKUT2WGBYQCCA',
+                          aws_secret_access_key='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')
+
+table = dynamodb.Table('People')
 
 app = Flask(__name__)
 
@@ -14,30 +21,28 @@ API Usage:
 
 @app.route('/hello/')
 @app.route('/hello')
+
 @app.route('/')
 def help():
     resp = Response(response=help_message,status=200,mimetype="text/plain")
     return resp
 
-@app.route('/hello/<name>', methods = ['GET','PUT'])
+@app.route('/hello/<name>', methods=['GET', 'PUT'])
 def hello(name):
-    if name is None:
-        return 'ECHO: No name supplied'
-    elif request.method == 'GET':
+    if request.method == 'GET':
         return find_in_store(name)
     elif request.method == 'PUT':
         return update_entry(name)
 
 def find_in_store(name):
-    filename = os.path.join(app.static_folder, 'people.json')
-    with open(filename) as people_file:
-        data = json.loads(people_file.read())
-        for i in data:
-            if i['name'] == name:
-                data = {"message": 'Hello, '+name+'!'+message(i['dateOfBirth'])}
-                resp = jsonify(data)
-                resp.status_code = 200
-                people_file.close()
+    resp = jsonify({"message": "This user does not exist."})
+    response = table.query(
+        KeyConditionExpression=Key('Username').eq(name)
+    )
+    for i in response['Items']:
+        data = {"message": 'Hello, ' + name + '!' + message(i['dateOfBirth'])}
+        resp = jsonify(data)
+        resp.status_code = 200
     return resp
 
 def message(birthdate):
@@ -46,22 +51,24 @@ def message(birthdate):
     if delta == 0:
         return ' Happy birthday!'
     else:
-        return ' Your birthday is in ' +str((delta+365)%365)+' days.'
+        return ' Your birthday is in ' + str((delta + 365) % 365) + ' days.'
 
-def update_entry(name):
-    filename = os.path.join(app.static_folder, 'people.json')
-    with open(filename, 'r+') as people_file:
-        data = json.loads(people_file.read())
-        update_req = request.json
-        for i in data:
-            if i['name'] == name:
-                i['dateOfBirth'] = update_req['dateOfBirth']
-                people_file.seek(0)
-                json.dump(data, people_file)
-                people_file.truncate()
-                people_file.close()
-                resp = Response(status=204, mimetype="application/json")
-        return resp
+def update_entry(user):
+    update_req = request.json
+    table.update_item(
+        Key={
+            'Username': user,
+        },
+        ReturnValues='UPDATED_NEW',
+        UpdateExpression="set dateOfBirth = :var",
+        ConditionExpression=Attr('Username').eq(user),
+        ExpressionAttributeValues={
+            ':var': update_req['dateOfBirth']
+        }
+    )
+
+    resp = Response(status=204, mimetype="application/json")
+    return resp
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
